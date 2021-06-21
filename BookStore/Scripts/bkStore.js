5cde21745548e9147cfb6d56cfcff4ca0df66f55
VS Unlock <vsunlock_bag@outlook.com>
2021-06-21 22:09:57 +0530

    Layout changes

---
+++ BookStore/Scripts/bkStore.js
@@ -0,0 +1,9 @@
+﻿
+
+function WindowResize() {
+    var widHt = $(window).height();
+    var headerHt = $("#header").height();
+    var footerHt = $("#footer").height();
+
+    $("#indexPage").height(widHt - headerHt - footerHt - 30);
+}
